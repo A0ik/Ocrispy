cada3358d948d93ae2dc25816669a4d6bbe0c7f9
Salman Bacherki <salmanbusiness.pro@gmail.com>
2025-12-28 16:07:08 +0100

    New website

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
       { protocol: "https", hostname: "randomuser.me" },
+      { protocol: "https", hostname: "assets.aceternity.com" },
     ],
   },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
